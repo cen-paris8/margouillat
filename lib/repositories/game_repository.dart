cbe808f98e2fe78eb5be9e007e33e0c0ee827eb9
Roger Jolly <jollyroger.apps@gmail.com>
2019-05-14 11:20:25 +0000

    [CODE] Clean code

--- lib/repositories/game_repository.dart
+++ lib/repositories/game_repository.dart
@@ -10,51 +10,12 @@ class GameRepository {
 
   Observable<Iterable<GameModel>> gameList() {
     Stream<QuerySnapshot> sqs = _firestoreProvider.gameListEvents();
-    /*
-    Future<QuerySnapshot> f = sqs.first;
-    f.then((QuerySnapshot q) { 
-      print(q); 
-      print(q.documents.first.documentID);
-      q.documents.forEach((DocumentSnapshot ds) {
-        print(ds.data['name']);
-      });
-    });
-    */
-
-    //Future<QuerySnapshot> f = sqs.first;
-    /*
-    f.then((QuerySnapshot q) { 
-      print(q); 
-      print(q.documents.first.documentID);
-      q.documents.forEach((DocumentSnapshot ds) {
-        print(ds.data['name']);
-      });
-    })
-    */
-    /*
-    f.then((QuerySnapshot qs) {
-      print(qs); 
-      print(qs.documents.first.documentID);
-      qs.documents.forEach((DocumentSnapshot ds) {
-        print(ds.data['name']);
-      });
-      //return new Observable.fromIterable(qs.documents.toList());
-      return new Observable(Stream.fromIterable(qs.documents.map(
-          (ds) => _toGameModel(ds)
-        )));
-      }
-    ).catchError(
-      return new Observable(Null);
-    );
-    */
     Future<Iterable<GameModel>> gameList = sqs.first.then(
      (qs) => qs.documents.map(
       (ds) => _toGameModel(ds)
       )
     );
-
     return new Observable.fromFuture(gameList);
-
   }
 
   GameModel _toGameModel(DocumentSnapshot snapshot) {
